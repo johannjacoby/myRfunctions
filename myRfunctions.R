eaf1772619141d2d4ece73c6be51d9786181a518
Johann Jacoby <johann.jacoby@gmail.com>
2020-07-17 15:51:04 +0200

    Update myRfunctions.R

--- myRfunctions.R
+++ myRfunctions.R
@@ -17,15 +17,23 @@ normtrunc <- function(x,m,sd,lower,upper, tol=1000000, progb = FALSE){
 
 
 breakText <- function(x, breakP=80){
-  x <- gsub("\\n","___lb___",x)
-  x <- gsub("\\t","___t___",x)
-  parts <- strsplit(gsub("  "," ",x), split=" ")[[1]]
-  ret <- ""
-  memory <- ""
-  for (i in parts){
-    if (nchar(memory) + nchar(i) < breakP) { ret <- paste0(ret,ifelse(ret=="",""," "),i); memory <- paste0(memory," ",i) }
-    else {ret <- paste0(ret, "\n",i); memory <- i}
+  rett <- ""
+  parags <- gsub("^ | $","",strsplit(x,split="\n")[[1]])
+  
+  for (p in parags){
+    xx<-p
+    xx <- gsub("\\t","___t___",xx)
+    parts <- strsplit(gsub("  "," ",xx), split=" ")[[1]]
+    ret <- ""
+    memory <- ""
+    for (i in parts){
+      if (nchar(memory) + nchar(i) < breakP) { ret <- paste0(ret,ifelse(ret=="",""," "),i); memory <- paste0(memory," ",i) }
+      else {ret <- paste0(ret, "\n",i); memory <- i}
+    }
+    rett <- paste0(rett, gsub("  "," ",gsub("___t___","\t",ret)))
   }
-  gsub("  "," ",gsub("___t___","\t",gsub("___lb___","\n",ret)))
+  rett
 }
 
+
+cat(breakText("huhu hafags jhgash db\n hdgfhgdhj dkhagsj ads", breakP=20))
